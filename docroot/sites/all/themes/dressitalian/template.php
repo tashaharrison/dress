2b76836fccd81b3c4fa5f35884bf94dd7587b278
Peter Ainsworth <pete@tashaharrison.co.uk>
2011-09-28 13:42:41 +0100

    Changed subtheme name from adaptivetheme_dressitalian to dressitalian and fixed template.php error

--- docroot/sites/all/themes/dressitalian/template.php
+++ docroot/sites/all/themes/dressitalian/template.php
@@ -2,25 +2,25 @@
 
 /**
  * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
- * 1. Rename each function and instance of "adaptivetheme_dressitalian" to match 
- *    your dressitalians name, e.g. if you name your theme "footheme" then the function
+ * 1. Rename each function and instance of "dressitalian" to match 
+ *    your subthemes name, e.g. if you name your theme "footheme" then the function
  *    name will be "footheme_preprocess_hook". Tip - you can search/replace 
- *    on "adaptivetheme_dressitalian".
+ *    on "dressitalian".
  * 2. Uncomment the required function to use.
  */
 
 /**
  * Override or insert variables into the html templates.
  */
-function adaptivetheme_dressitalian_preprocess_html(&$vars) {
+function dressitalian_preprocess_html(&$vars) {
   // Load the media queries styles
   // Remember to rename these files to match the names used here - they are 
-  // in the CSS directory of your dressitalian.
+  // in the CSS directory of your subtheme.
   $media_queries_css = array(
-    'adaptivetheme_dressitalian.responsive.style.css',
-    'adaptivetheme_dressitalian.responsive.gpanels.css'
+    'dressitalian.responsive.style.css',
+    'dressitalian.responsive.gpanels.css'
   );
-  load_dressitalian_media_queries($media_queries_css, 'adaptivetheme_dressitalian');
+  load_subtheme_media_queries($media_queries_css, 'dressitalian');
 
  /**
   * Load IE specific stylesheets
@@ -33,18 +33,18 @@ function adaptivetheme_dressitalian_preprocess_html(&$vars) {
   *
   *  'IE 8' => 'ie-8.css',
   *
-  * Your IE CSS file must be in the /css/ directory in your dressitalian.
+  * Your IE CSS file must be in the /css/ directory in your subtheme.
   */
   /* -- Delete this line to add a conditional stylesheet for IE 7 or less.
   $ie_files = array(
     'lte IE 7' => 'ie-lte-7.css',
   );
-  load_dressitalian_ie_styles($ie_files, 'adaptivetheme_dressitalian');
+  load_subtheme_ie_styles($ie_files, 'dressitalian');
   // */
 }
 
 /* -- Delete this line if you want to use this function
-function adaptivetheme_dressitalian_process_html(&$vars) {
+function dressitalian_process_html(&$vars) {
 }
 // */
 
@@ -52,10 +52,10 @@ function adaptivetheme_dressitalian_process_html(&$vars) {
  * Override or insert variables into the page templates.
  */
 /* -- Delete this line if you want to use these functions
-function adaptivetheme_dressitalian_preprocess_page(&$vars) {
+function dressitalian_preprocess_page(&$vars) {
 }
 
-function adaptivetheme_dressitalian_process_page(&$vars) {
+function dressitalian_process_page(&$vars) {
 }
 // */
 
@@ -63,10 +63,10 @@ function adaptivetheme_dressitalian_process_page(&$vars) {
  * Override or insert variables into the node templates.
  */
 /* -- Delete this line if you want to use these functions
-function adaptivetheme_dressitalian_preprocess_node(&$vars) {
+function dressitalian_preprocess_node(&$vars) {
 }
 
-function adaptivetheme_dressitalian_process_node(&$vars) {
+function dressitalian_process_node(&$vars) {
 }
 // */
 
@@ -74,10 +74,10 @@ function adaptivetheme_dressitalian_process_node(&$vars) {
  * Override or insert variables into the comment templates.
  */
 /* -- Delete this line if you want to use these functions
-function adaptivetheme_dressitalian_preprocess_comment(&$vars) {
+function dressitalian_preprocess_comment(&$vars) {
 }
 
-function adaptivetheme_dressitalian_process_comment(&$vars) {
+function dressitalian_process_comment(&$vars) {
 }
 // */
 
@@ -85,16 +85,16 @@ function adaptivetheme_dressitalian_process_comment(&$vars) {
  * Override or insert variables into the block templates.
  */
 /* -- Delete this line if you want to use these functions
-function adaptivetheme_dressitalian_preprocess_block(&$vars) {
+function dressitalian_preprocess_block(&$vars) {
 }
 
-function adaptivetheme_dressitalian_process_block(&$vars) {
+function dressitalian_process_block(&$vars) {
 }
 // */
 
 /**
  * Add the Style Schemes if enabled.
- * NOTE: You MUST make changes in your dressitalians theme-settings.php file
+ * NOTE: You MUST make changes in your subthemes theme-settings.php file
  * also to enable Style Schemes.
  */
 /* -- Delete this line if you want to enable style schemes.
