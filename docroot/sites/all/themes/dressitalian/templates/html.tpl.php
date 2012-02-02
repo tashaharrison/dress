d6e9169ca6fe98ff459a0df9ab3e88e1dde04787
Peter Ainsworth <pete@peteainsworth.com>
2012-02-02 19:12:28 +0000

    Modified html.tpl.php to display metatags on nodes

--- docroot/sites/all/themes/dressitalian/templates/html.tpl.php
+++ docroot/sites/all/themes/dressitalian/templates/html.tpl.php
@@ -1,3 +1,9 @@
+<?php 
+if ((arg(0) == 'node') && is_numeric(arg(1))) {
+  $node = node_load(arg(1));
+  $metatags = metatag_metatags_load('node',$node->nid);
+}
+?>
 <!DOCTYPE html>
 <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (lte IE 6)&(!IEMobile)]><html class="ie6 ie6-7 ie6-8" <?php print $html_attributes; ?>><![endif]-->
@@ -12,7 +18,10 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta http-equiv="cleartype" content="on">
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-<title><?php print $head_title; ?></title>
+<title><?php if (!empty($metatags['title']['value'])) { print $metatags['title']['value']; } else { print $head_title; } ?></title>
+<?php if (!empty($metatags['keywords']['value'])) { ?><meta name="keywords" content="<?php print $metatags['keywords']['value']; ?>" />
+<?php } if (!empty($metatags['description']['value'])) { ?><meta name="description" content="<?php print $metatags['description']['value']; ?>" />
+<?php } ?>
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
