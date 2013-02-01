f1ff8127d311bc8d561f1dfc7737a09f626946ba
Tasha Harrison <tasha@spirelightmedia.com>
2013-02-01 16:19:34 +0000

    Change to blog sidebar so only categories is position fixed

--- sites/all/themes/dressitalianresponsive/js/script.js
+++ sites/all/themes/dressitalianresponsive/js/script.js
@@ -1,6 +1,6 @@
 (function($) {
 	$(document).ready(function(){
-	var sticky_container = $('#region-sidebar-second');
+	var sticky_container = $('#block-menu-menu-travel-blog-categories');
 	
 	sticky_container.waypoint(function(direction) {
 		sticky_container.toggleClass("sticky",direction==='down');
